813489b23449c6e37407a20af3b24097793dc8cf
Deepak Kumar Yadav <98113881+2003deepak@users.noreply.github.com>
2025-05-08 02:23:38 +0530

    Update config.php

--- website/config.php
+++ website/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website , Changes Done By Deepak at 02:11 AM',
+        'name' => 'Simple PHP Website',
         'nav_menu' => [
             '' => 'Home',
             'content/about-us.php' => 'About Us',
